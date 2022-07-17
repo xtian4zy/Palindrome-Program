d254efc1e6c823ef2f52a0c17ac0dddce2839105
xtian4zy <xtianidemudia@yahoo.co.uk>
2022-07-17 05:36:09 +0100

    Checking string for palindrome

--- Palindrome program.py
+++ Palindrome program.py
@@ -1,10 +1,10 @@
-text_check = 'mum'  
-strstr = text_check.casefold()  
+text_check = 'mum'  #The string you want to test
+strstr = text_check.casefold()  #returns the string with all the characters in lower case.
   
  
-rev = reversed(text_check)  
+rev = reversed(text_check)  # Reversing the string. 
   
-if list(text_check) == list(rev):  
+if list(text_check) == list(rev):  #Making the decision whether string is palindrome or not.
    print("The string is a Palindrome")  
 else:  
    print("The string is not a Palindrome")  
